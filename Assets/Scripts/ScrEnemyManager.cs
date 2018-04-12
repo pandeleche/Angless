46b3f972f0b93c0c96b4305bfcde615f7aa160d2
Termostatos <4ferluj@gmail.com>
2018-04-12 16:50:23 +0200

    Enemy follows the lider

---
+++ Assets/Scripts/ScrEnemyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrEnemyManager : MonoBehaviour {
+
+    public GameObject square;
+    public float spawnTime = 3f;
+    public Transform[] spawnPoints;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Spawn()
+    {
+        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
+        GameObject saux = Instantiate(square, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+        saux.AddComponent<EnemyBehaviour>();
+    }
+}
+
