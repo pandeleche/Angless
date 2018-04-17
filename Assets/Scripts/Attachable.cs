274803de9ec52fd329d2febb2400bd65581b5eff
Pandeleche <curromakinon@hotmail.com>
2018-04-17 12:25:51 +0100

    Attachable script edited

---
+++ Assets/Scripts/Attachable.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Attachable : MonoBehaviour {
+	
+	public int interactiveLayer = 10;
+	public 	Collider myCollider;
+	public Hand controllerScript;
+	public GameObject sword;
+
+	private bool attached;
+	private GameObject theClosestGO;
+	// Use this for initialization
+	void Start () {
+		attached = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void DoClick()
+	{
+		if (theClosestGO != null) {
+			attached = true;
+			controllerScript.AttachObject (sword);
+		}
+	}
+	void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.layer == interactiveLayer)
+		{
+			theClosestGO = collision.gameObject;
+		}
+	}
+	void OnTriggerExit(Collider collision)
+	{
+		theClosestGO = null;
+	}
+}
