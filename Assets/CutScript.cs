f4880b83f8b141222d4a4670e5f6ed0ca9797b69
Termostatos <4ferluj@gmail.com>
2018-04-26 19:23:17 +0200

    Now they die

---
+++ Assets/CutScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Cutable"))
+        {
+            other.gameObject.GetComponentInChildren<ParticleSystem>().Play();
+            Destroy(other.gameObject,2f);
+        }
+    }
+}
