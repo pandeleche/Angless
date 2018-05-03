33cd8860dc56555843ce28bb166ea1273b248219
Pandeleche <curromakinon@hotmail.com>
2018-05-03 18:38:46 +0200

    Add score and blood sprite

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	public Text text_score;
+	private int score;
+	private float perfect_cut_cos = Mathf.Cos(Mathf.PI/4);
+	// Use this for initialization
+	void Start () {
+		score = 100;
+	}
+
+	void CutDone(Vector3 rotation){
+		int increasing = Mathf.RoundToInt(Mathf.Abs(Mathf.Abs(Mathf.Cos(Mathf.Deg2Rad * rotation.z)) - Mathf.Abs(Mathf.Cos (Mathf.Deg2Rad * rotation.z)))*100);
+		score += increasing;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text_score.text = "Score: " + score;
+	}
+}
---
+++ Assets/Scripts/TransparencyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransparencyScript : MonoBehaviour {
+
+	public Image image;
+	public float transparency;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Color color = image.material.color;
+		color.a = transparency;
+		image.material.color = color;
+	}
+}
