b27e1dbbfda584c3b4505b953477279ef30a1a6f
Pandeleche <curromakinon@hotmail.com>
2018-05-07 19:24:04 +0200

    Added damage and lose message

---
+++ Assets/Scripts/LoseMessage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoseMessage : MonoBehaviour {
+
+	public Text Screen_canvas_text;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void Player_lose(){
+		Screen_canvas_text.text = "YOU LOSE";
+		Screen_canvas_text.fontSize = 50;
+	}
+}
---
+++ Assets/Scripts/ReceiveDamage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReceiveDamage : MonoBehaviour {
+
+	private int Heatlh_Points = 5;
+	public LoseMessage lose;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag.Equals("Cutable")) {
+			Heatlh_Points--;
+		}
+		if (Heatlh_Points == 0) {
+			lose.Invoke ("Player_lose",1);
+		}
+	}
+}
