5816a431b8f1dc1021baea309e01e0de69e9e477
Termostatos <4ferluj@gmail.com>
2018-05-17 17:04:32 +0200

    more music in scenes

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -3,19 +3,24 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour {
 
+    
+
 	public void Start(){
+       
 		DynamicGI.UpdateEnvironment();
 	}
 	public void LoadGame(string scene_name)
 	{
-		SceneManager.LoadScene(scene_name, LoadSceneMode.Single);
+        
+        SceneManager.LoadScene(scene_name, LoadSceneMode.Single);
 	}
 	public void RestartGame(){
-		SceneManager.LoadScene (SceneManager.GetActiveScene ().name,LoadSceneMode.Single);
+       
+        SceneManager.LoadScene (SceneManager.GetActiveScene ().name,LoadSceneMode.Single);
 	}
 	public void ExitGame()
 	{
-		Debug.Log("QUIT!!!");
+        Debug.Log("QUIT!!!");
 		Application.Quit();
 	}
 }
